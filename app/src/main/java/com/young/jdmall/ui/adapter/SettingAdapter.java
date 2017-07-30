95da36f5f1448cb9531e2e207f09ec69d1b308e0
Young <zhipengzhong@live.com>
2017-07-31 00:24:45 +0800

    通过第三方控件 MagicIndicator 实现ViePager 的 Tab

---
+++ app/src/main/java/com/young/jdmall/ui/adapter/SettingAdapter.java
@@ -0,0 +1,38 @@
+package com.young.jdmall.ui.adapter;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by 钟志鹏 on 2017/7/30.
+ */
+
+public class SettingAdapter extends PagerAdapter {
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        TextView textView = new TextView(container.getContext());
+        textView.setText("1");
+        container.addView(textView);
+        return textView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((TextView) object);
+    }
+
+    @Override
+    public int getCount() {
+        return 10;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
--- app/src/main/java/com/young/jdmall/ui/fragment/SettingFragment.java
+++ app/src/main/java/com/young/jdmall/ui/fragment/SettingFragment.java
@@ -1,12 +1,32 @@
 package com.young.jdmall.ui.fragment;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.young.jdmall.R;
+import com.young.jdmall.ui.adapter.SettingAdapter;
+
+import net.lucode.hackware.magicindicator.FragmentContainerHelper;
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by 钟志鹏 on 2017/7/30.
@@ -14,11 +34,65 @@ import android.widget.TextView;
 
 public class SettingFragment extends Fragment {
 
+    @BindView(R.id.view_pager)
+    ViewPager mViewPager;
+    Unbinder unbinder;
+    @BindView(R.id.magic_indicator)
+    MagicIndicator mMagicIndicator;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText("设置");
-        return textView;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getActivity().getWindow().setStatusBarColor(0x66000000);
+        }
+        View view = View.inflate(getActivity(), R.layout.fragment_find, null);
+        unbinder = ButterKnife.bind(this, view);
+        setAdapter();
+        return view;
+    }
+
+    private void setAdapter() {
+        mViewPager.setAdapter(new SettingAdapter());
+        CommonNavigator commonNavigator = new CommonNavigator(getActivity());
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+            @Override
+            public int getCount() {
+                return 10;
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                ColorTransitionPagerTitleView colorTransitionPagerTitleView = new ColorTransitionPagerTitleView(context);
+                colorTransitionPagerTitleView.setNormalColor(Color.GRAY);
+                colorTransitionPagerTitleView.setSelectedColor(Color.RED);
+                colorTransitionPagerTitleView.setText("精选");
+                colorTransitionPagerTitleView.setTextSize(12);
+                colorTransitionPagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mViewPager.setCurrentItem(index);
+                    }
+                });
+                return colorTransitionPagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                LinePagerIndicator indicator = new LinePagerIndicator(context);
+                indicator.setMode(LinePagerIndicator.MODE_WRAP_CONTENT);
+                indicator.setColors(Color.RED);
+                return indicator;
+            }
+        });
+
+        mMagicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(mMagicIndicator, mViewPager);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
