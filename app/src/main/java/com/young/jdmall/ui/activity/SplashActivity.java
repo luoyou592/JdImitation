358413e1dcef941288be24384f656d16998be28e
âbob <506764102@qq.com>
2017-08-02 16:12:47 +0800

    引导页面完成（前面代码丢失，做个简易版的）

---
+++ app/src/main/java/com/young/jdmall/ui/activity/SplashActivity.java
@@ -0,0 +1,100 @@
+package com.young.jdmall.ui.activity;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.young.jdmall.R;
+import com.young.jdmall.ui.adapter.SplashAdapter;
+import com.young.jdmall.ui.fragment.SplashFirstFragment;
+import com.young.jdmall.ui.fragment.SplashThirdFragment;
+import com.young.jdmall.ui.fragment.SplashTwoFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by BjyJyk on 2017/7/31.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = "SplashActivity";
+    private List<Fragment> fragments=new ArrayList<>();
+    @BindView(R.id.vp_splash)
+    ViewPager mVpSplash;
+    private FragmentManager mSupportFragmentManager;
+    private int mlastPosition=0;
+    private int mPosition;
+
+    //  private android.app.FragmentManager mFragmentManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+        initdata();
+        init();
+    }
+
+    private void initdata() {
+        Fragment fragment1=new SplashFirstFragment();
+        Fragment fragment2=new SplashTwoFragment();
+        Fragment fragment3=new SplashThirdFragment();
+        mSupportFragmentManager = getSupportFragmentManager();
+
+
+        fragments.add(fragment1);
+        fragments.add(fragment2);
+        fragments.add(fragment3);
+
+    }
+
+    private void init() {
+        SplashAdapter splashAdapter=new SplashAdapter(mSupportFragmentManager,fragments);
+        mVpSplash.setAdapter(splashAdapter);
+        mVpSplash.setOnPageChangeListener(mOnPageChangeListener);
+    }
+
+
+    private ViewPager.OnPageChangeListener mOnPageChangeListener=new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            Log.e(TAG, "onPageScrolled: "+positionOffsetPixels );
+
+            ((SplashFirstFragment)fragments.get(0)) .ScrollView(positionOffsetPixels-mlastPosition);
+             mlastPosition = positionOffsetPixels;
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            mPosition = position;
+            if(position==2){
+                ((SplashThirdFragment)fragments.get(2)).startLoop();
+            }
+
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+            if(mPosition==1){
+                if(state==2){
+                  ((SplashTwoFragment)fragments.get(1)).mRotationAnim.start();
+                }else if(state==0){
+                    ((SplashTwoFragment)fragments.get(1)).mRotationAnim.clone();
+                }
+            }
+
+        }
+    };
+}
