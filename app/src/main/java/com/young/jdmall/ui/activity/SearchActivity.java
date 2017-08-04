44c93619c6c3855d5d09c88c2cfdc8ff7bb6c8c2
Young <zhipengzhong@live.com>
2017-08-04 18:09:57 +0800

    完成搜索界面及功能

--- app/src/main/java/com/young/jdmall/ui/activity/SearchActivity.java
+++ app/src/main/java/com/young/jdmall/ui/activity/SearchActivity.java
@@ -1,5 +1,7 @@
 package com.young.jdmall.ui.activity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,11 +16,13 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.young.jdmall.R;
 import com.young.jdmall.bean.HotSearchInfoBean;
 import com.young.jdmall.network.NetworkManage;
+import com.young.jdmall.ui.utils.PreferenceUtils;
 import com.young.jdmall.ui.view.FlowLayout;
 
 import java.util.List;
@@ -46,6 +50,14 @@ public class SearchActivity extends AppCompatActivity {
     FlowLayout mSearchFlowLayout;
     @BindView(R.id.search_linear)
     LinearLayout mSearchLinear;
+    @BindView(R.id.search_clean_button)
+    LinearLayout mSearchCleanButton;
+    @BindView(R.id.type_1)
+    LinearLayout mType1;
+    @BindView(R.id.type_2)
+    RelativeLayout mType2;
+    @BindView(R.id.search_linear_history)
+    LinearLayout mSearchLinearHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +71,20 @@ public class SearchActivity extends AppCompatActivity {
         initView();
         setListener();
         loadData();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        List<String> searchList = PreferenceUtils.getSearchList(this);
+        if (searchList != null) {
+            loadDataToTypeSearchList(searchList);
+            mType2.setVisibility(View.VISIBLE);
+            mType1.setVisibility(View.GONE);
+        } else {
+            mType1.setVisibility(View.VISIBLE);
+            mType2.setVisibility(View.GONE);
+        }
     }
 
     private void loadData() {
@@ -81,7 +107,7 @@ public class SearchActivity extends AppCompatActivity {
         List<String> searchKeywords = body.getSearchKeywords();
 
         for (int i = 0; i < 1; i++) {
-            for (String searchKeyword : searchKeywords) {
+            for (final String searchKeyword : searchKeywords) {
                 TextView textView = new TextView(this);
                 textView.setText(searchKeyword);
                 textView.setBackgroundResource(R.drawable.search_flow_item_gb);
@@ -90,6 +116,12 @@ public class SearchActivity extends AppCompatActivity {
                 int padding10 = getResources().getDimensionPixelSize(R.dimen.dp_10);
                 textView.setPadding(padding10, padding2, padding10, padding2);
                 textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13);
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(searchKeyword);
+                    }
+                });
                 LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 int margin5 = getResources().getDimensionPixelSize(R.dimen.dp_5);
                 layoutParams.setMargins(margin5, 0, margin5, 0);
@@ -101,8 +133,8 @@ public class SearchActivity extends AppCompatActivity {
     private void loadDataToFlowLayout(HotSearchInfoBean body) {
         List<String> searchKeywords = body.getSearchKeywords();
 
-        for (int i = 0; i < 1; i++) {
-            for (String searchKeyword : searchKeywords) {
+        for (int i = 0; i < 2; i++) {
+            for (final String searchKeyword : searchKeywords) {
                 TextView textView = new TextView(this);
                 textView.setText(searchKeyword);
                 textView.setBackgroundResource(R.drawable.search_flow_item_gb);
@@ -111,9 +143,49 @@ public class SearchActivity extends AppCompatActivity {
                 int padding10 = getResources().getDimensionPixelSize(R.dimen.dp_10);
                 textView.setPadding(padding10, padding2, padding10, padding2);
                 textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 13);
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(searchKeyword);
+                    }
+                });
                 mSearchFlowLayout.addView(textView);
             }
         }
+    }
+
+
+    private void loadDataToTypeSearchList(List<String> searchList) {
+        int padding = getResources().getDimensionPixelSize(R.dimen.dp_14);
+        mSearchLinearHistory.removeAllViews();
+        for (int i = 0; i < searchList.size(); i++) {
+            if (i != 0) {
+                View view = new View(this);
+                view.setBackgroundColor(0xFFEEEEEE);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 2);
+                layoutParams.setMargins(padding, 0, 0, 0);
+                mSearchLinearHistory.addView(view, layoutParams);
+            }
+            final String s = searchList.get(i);
+            TextView textView = new TextView(this);
+            textView.setText(s);
+            textView.setPadding(padding, padding, padding, padding);
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(s);
+                }
+            });
+            mSearchLinearHistory.addView(textView);
+        }
+    }
+
+    private void startActivity(String searchKeyword) {
+        Intent intent = new Intent(SearchActivity.this, TypeActivity.class);
+        intent.putExtra("name", searchKeyword);
+        startActivity(intent);
+        PreferenceUtils.addSearchList(SearchActivity.this, searchKeyword);
     }
 
     private void setListener() {
@@ -143,7 +215,40 @@ public class SearchActivity extends AppCompatActivity {
         mSearchClean.setEnabled(false);
     }
 
-    @OnClick({R.id.search_back, R.id.search_clean, R.id.search_submit})
+    private void showIsCleanDialog() {
+        View view = View.inflate(this, R.layout.layout_serch_dialog, null);
+        View confirm = view.findViewById(R.id.confirm);
+        View abolish = view.findViewById(R.id.abolish);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setView(view)
+                .create();
+        alertDialog.show();
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PreferenceUtils.deleteSearchList(SearchActivity.this);
+                mType1.setVisibility(View.VISIBLE);
+                mType2.setVisibility(View.GONE);
+                alertDialog.dismiss();
+            }
+        });
+        abolish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+//        Dialog dialog = new Dialog(this);
+//        dialog.setContentView(view);
+//        dialog.show();
+
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.
+
+    }
+
+    @OnClick({R.id.search_back, R.id.search_clean, R.id.search_submit, R.id.search_clean_button})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.search_back:
@@ -153,6 +258,10 @@ public class SearchActivity extends AppCompatActivity {
                 mSearchInput.setText("");
                 break;
             case R.id.search_submit:
+                startActivity(TextUtils.isEmpty(mSearchInput.getText().toString().trim()) ? "8.4帮宝适超级新品日" : mSearchInput.getText().toString().trim());
+                break;
+            case R.id.search_clean_button:
+                showIsCleanDialog();
                 break;
         }
     }
--- app/src/main/java/com/young/jdmall/ui/utils/PreferenceUtils.java
+++ app/src/main/java/com/young/jdmall/ui/utils/PreferenceUtils.java
@@ -2,6 +2,14 @@ package com.young.jdmall.ui.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  *  创建者:   tiao
@@ -9,17 +17,21 @@ import android.content.SharedPreferences;
  *  描述：    TODO
  */
 public class PreferenceUtils {
+
+    private static final String TAG = "PreferenceUtils";
+
     private static final String NAME = "jdmall";
-    private static final String KEY_USER_NAME= "user_name";
+    private static final String KEY_USER_NAME = "user_name";
     private static final String KEY_USER_ID = "user_id";
     private static final String KEY_REGIST_SUCCESS = "regist_success";
+    private static final String KEY_SEARCH_LIST = "search_list";
 
-    public static void setUserName(Context context, String value){
+    public static void setUserName(Context context, String value) {
         SharedPreferences sp = getPreference(context);
         sp.edit().putString(KEY_USER_NAME, value).commit();
     }
 
-    public static String getUserName(Context context){
+    public static String getUserName(Context context) {
         SharedPreferences sp = getPreference(context);
         return sp.getString(KEY_USER_NAME, "");
     }
@@ -29,25 +41,76 @@ public class PreferenceUtils {
         return sp;
     }
 
-    public static void setUserId(Context context, String value){
+    public static void setUserId(Context context, String value) {
         SharedPreferences sp = getPreference(context);
         sp.edit().putString(KEY_USER_ID, value).commit();
     }
 
-    public static String getUserId(Context context){
+    public static String getUserId(Context context) {
         SharedPreferences sp = getPreference(context);
         return sp.getString(KEY_USER_ID, "");
     }
 
-    public static void setRegistSuccess(Context context, boolean value){
+    public static void setRegistSuccess(Context context, boolean value) {
         SharedPreferences sp = getPreference(context);
         sp.edit().putBoolean(KEY_REGIST_SUCCESS, value).commit();
     }
 
 
-    public static boolean getRegistSuccess(Context context){
+    public static boolean getRegistSuccess(Context context) {
         SharedPreferences sp = getPreference(context);
         return sp.getBoolean(KEY_REGIST_SUCCESS, false);
+    }
+
+    public static List<String> getSearchList(Context context) {
+        SharedPreferences preference = getPreference(context);
+        String string = preference.getString(KEY_SEARCH_LIST, "");
+        if (!TextUtils.isEmpty(string)) {
+            Gson gson = new Gson();
+            List<String> strings = gson.fromJson(string, new TypeToken<List<String>>() {
+            }.getType());
+            return strings;
+        }
+        return null;
+    }
+
+    public static void setSearchList(Context context, List<String> list) {
+        SharedPreferences pf = getPreference(context);
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        Log.d(TAG, "setSearchList: " + json);
+        pf.edit().putString(KEY_SEARCH_LIST, json).commit();
+    }
+
+    public static void addSearchList(Context context, String str) {
+        if (!TextUtils.isEmpty(str)) {
+            SharedPreferences preference = getPreference(context);
+            String string = preference.getString(KEY_SEARCH_LIST, "");
+            List<String> strings;
+            if (!TextUtils.isEmpty(string)) {
+                Gson gson = new Gson();
+                strings = gson.fromJson(string, new TypeToken<List<String>>() {
+                }.getType());
+            } else {
+                strings = new ArrayList<>();
+            }
+            for (String s : strings) {
+                if (str.equals(s)) {
+                    return;
+                }
+            }
+            strings.add(str);
+            SharedPreferences pf = getPreference(context);
+            Gson gson = new Gson();
+            String json = gson.toJson(strings);
+            Log.d(TAG, "setSearchList: " + json);
+            pf.edit().putString(KEY_SEARCH_LIST, json).commit();
+        }
+    }
+
+    public static void deleteSearchList(Context context) {
+        SharedPreferences pf = getPreference(context);
+        pf.edit().putString(KEY_SEARCH_LIST, "").commit();
     }
 
 }
