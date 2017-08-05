7aab1c1e509d7cbff226d4c4401b911805829f54
Tony <tony@gmail.com>
2017-08-05 20:39:13 +0800

    分类页面搜索框

--- app/src/main/java/com/young/jdmall/ui/fragment/CategoryFragment.java
+++ app/src/main/java/com/young/jdmall/ui/fragment/CategoryFragment.java
@@ -1,6 +1,7 @@
 package com.young.jdmall.ui.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
@@ -10,12 +11,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import com.rance.chatui.ui.activity.MainActivity;
 import com.young.jdmall.R;
 import com.young.jdmall.bean.CategoryBaseBean;
 import com.young.jdmall.network.BaseObserver;
 import com.young.jdmall.network.RetrofitFactory;
+import com.young.jdmall.ui.activity.SearchActivity;
 import com.young.jdmall.ui.adapter.CategoryLeftAdapter;
 
 import java.util.ArrayList;
@@ -40,17 +45,25 @@ public class CategoryFragment extends BaseFragment {
     public RecyclerView mCategoryLeftList;
     @BindView(R.id.category_right_contains)
     FrameLayout mCategoryRightContains;
+    @BindView(R.id.et_search_catecary)
+    EditText mEtSearch;
+    @BindView(R.id.tv_news_category)
+    TextView mTvMsg;
     private Context mContext;
     private CategoryLeftAdapter mCategoryLeftAdapter;
     private CategoryBaseBean mCategoryBean;
     private List<CategoryBaseRightListFragment> fragments = new ArrayList();
+    private EditText mViewById;
 
     @RequiresApi(api = M)
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootview = LayoutInflater.from(getActivity()).inflate(R.layout.catecary_fragment, null);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
+        View rootview = LayoutInflater.from(getActivity()).inflate(R.layout.catecary_fragment,
+                null);
         ButterKnife.bind(this, rootview);
+
         mContext = getActivity();
         //把代码复制过来
         init();
@@ -61,7 +74,7 @@ public class CategoryFragment extends BaseFragment {
     @Override
     public void onResume() {
         super.onResume();
-        if (fragments.size() != 0){
+        if (fragments.size() != 0) {
             selectFragment(1);
         }
     }
@@ -84,28 +97,45 @@ public class CategoryFragment extends BaseFragment {
 
     public void selectFragment(int position) {
         CategoryBaseRightListFragment baseCategoryRightFragment = fragments.get(position);
-        getFragmentManager().beginTransaction().replace(R.id.category_right_contains, baseCategoryRightFragment).commit();
-        if (position >4) {
+        getFragmentManager().beginTransaction().replace(R.id.category_right_contains,
+                baseCategoryRightFragment).commit();
+        if (position > 4) {
             if (lastcheck < position) {
-                mCategoryLeftList.smoothScrollToPosition(position +4);
+                mCategoryLeftList.smoothScrollToPosition(position + 4);
                 lastcheck = position;
             } else {
                 lastcheck = position;
-                  mCategoryLeftList.smoothScrollToPosition(position -4 );
+                mCategoryLeftList.smoothScrollToPosition(position - 4);
             }
-        }else {
+        } else {
             mCategoryLeftList.smoothScrollToPosition(0);
         }
 
 
-        }
+    }
 
-        /**
-         * 初始化
-         */
+    /**
+     * 初始化
+     */
 
     private void init() {
-        Log.d(TAG, "init: 初始化");
+        mEtSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().startActivity(new Intent(getActivity(), SearchActivity.class));
+            }
+        });
+
+        mTvMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                getActivity().startActivity(intent);
+
+            }
+        });
+
+
         //加载网络数据
         loadNetwork();
 
@@ -118,40 +148,24 @@ public class CategoryFragment extends BaseFragment {
 
     private void loadNetwork() {
         Log.d(TAG, "loadNetwork: 加载数据");
-        Observable<CategoryBaseBean> categoryObservable = RetrofitFactory.getInstance().listCategory();
-        categoryObservable.compose(compose(this.<CategoryBaseBean>bindToLifecycle())).subscribe(new BaseObserver<CategoryBaseBean>(getActivity()) {
-            @Override
-            protected void onHandleSuccess(CategoryBaseBean categoryBean) {
-                mCategoryBean = categoryBean;
-                Log.d(TAG, "onHandleSuccess: 加载数据成功");
-                //加载成功再添加Fragment
-                initFragment();
-                selectFragment(0);
-            }
-            @Override
-            protected void onHandleError(String msg) {
-                Log.d(TAG, "onHandleError: 加载失败");
-            }
-        });
-//        categoryObservable.compose(compose(this.<CategoryBaseBean>bindToLifecycle())).subscribe(new BaseObserver
-//                <CategoryBaseBean>(getActivity()) {
-//            @Override
-//            protected void onHandleSuccess(CategoryBaseBean categoryBaseBean) {
-//                Log.d(TAG, "onHandleSuccess: 请求数据成功");
-//            }
-//
-//            @Override
-//            protected void onHandleError(String msg) {
-//                Log.d(TAG, "onHandleError: 请求数据失败");
-//            }
-//        });
+        Observable<CategoryBaseBean> categoryObservable = RetrofitFactory.getInstance()
+                .listCategory();
+        categoryObservable.compose(compose(this.<CategoryBaseBean>bindToLifecycle())).subscribe
+                (new BaseObserver<CategoryBaseBean>(getActivity()) {
+                    @Override
+                    protected void onHandleSuccess(CategoryBaseBean categoryBean) {
+                        mCategoryBean = categoryBean;
+                        Log.d(TAG, "onHandleSuccess: 加载数据成功");
+                        //加载成功再添加Fragment
+                        initFragment();
+                        selectFragment(0);
+                    }
+
+                    @Override
+                    protected void onHandleError(String msg) {
+                        Log.d(TAG, "onHandleError: 加载失败");
+                    }
+                });
     }
 
-
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-    }
-    
 }
